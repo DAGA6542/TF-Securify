532894cb4e65e9ac954b808f4f0dfcdb609caa3a
DAGA6542 <U202120374@upc.edu.pe>
2025-05-07 23:58:47 -0500

    Creacion de archivo

---
+++ src/test/java/org/example/securify_backend/SecurifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.securify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
