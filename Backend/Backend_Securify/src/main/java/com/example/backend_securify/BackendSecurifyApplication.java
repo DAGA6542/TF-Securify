1ec04a4b1225be049aa95e526772fd1a4fc68b95
DAGA6542 <u202120374@upc.edu.pe>
2025-05-09 08:51:26 -0500

    Primer commit del proyecto en rama diego

---
+++ Backend/Backend_Securify/src/main/java/com/example/backend_securify/BackendSecurifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_securify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSecurifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSecurifyApplication.class, args);
+    }
+
+}
---
+++ Backend/Backend_Securify/src/test/java/com/example/backend_securify/BackendSecurifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_securify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSecurifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
